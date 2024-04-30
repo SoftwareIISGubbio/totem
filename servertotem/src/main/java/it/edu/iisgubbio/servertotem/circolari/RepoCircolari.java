ad84e2de8c49ae11522470195136200e91a84067
unknown <alessandro.satiri@iisgubbio.edu.it>
2024-04-30 15:29:19 +0200

    Funzionamento server circolari

---
+++ servertotem/src/main/java/it/edu/iisgubbio/servertotem/circolari/RepoCircolari.java
@@ -0,0 +1,7 @@
+package it.edu.iisgubbio.servertotem.circolari;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepoCircolari extends JpaRepository<Circolari, Integer>{
+
+}
