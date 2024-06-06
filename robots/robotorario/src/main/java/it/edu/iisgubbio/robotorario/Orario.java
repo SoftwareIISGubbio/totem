b62277b3c9c9452d5e5bfad0dda6ae5ffa7da5f7
yassedboussalham <yassedboussalham@air02.local>
2024-06-06 09:46:33 +0200

    progettoAl99%

--- robots/robotorario/src/main/java/it/edu/iisgubbio/robotorario/Orario.java
+++ robots/robotorario/src/main/java/it/edu/iisgubbio/robotorario/Orario.java
@@ -3,95 +3,89 @@ package it.edu.iisgubbio.robotorario;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.Scanner;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Orario {
-	private static String getValore(String variabile, String predefinito){
-		String v = System.getenv(variabile);
-		if(v==null){
-			return predefinito;
-		} else {
-			return v;
-		}
-	}
-	public static void main(String[] args) throws Exception {
-		// URL della sezione "Tecnologia" del sito web "[URL non valido rimosso]"
-		final String URL_ORARI = "https://lnx.iisgubbio.edu.it/Orario/orario_docenti_nuovo.html";
-		Document doc = Jsoup.connect(URL_ORARI).get();
+    private static String getValore(String variabile, String predefinito) {
+        String v = System.getenv(variabile);
+        return (v == null) ? predefinito : v;
+    }
 
-		// viene dopo p
-		// Elements titoli = doc.select("p + table");
-		// oppure la classe finisce con _table
-		Elements tabelle = doc.select("table[class$=\"_table\"]");
+    public static void main(String[] args) throws Exception {
+        final String URL_ORARI = "https://lnx.iisgubbio.edu.it/Orario/orario_docenti_nuovo.html";
+        Document doc = Jsoup.connect(URL_ORARI).get();
 
-		for (Element tabella : tabelle) {
-			Element docente = tabella.select("thead tr:first-child").get(0);
-			System.out.println(docente.text());
-		}
-		System.out.println("Inserisci il nome del docente per visualizzare l'orario:");
-		// String nomeDocente = scanner.nextLine();
-		String nomeDocente;
-		String classe;
-		String aula ;
-		String insert;
-		String url = getValore("TOTEM_DATABASE","jdbc:mysql://10.1.0.52:3306/totem");
-		String username = "totem";
-		String password = "totem";
-		try (Connection connection = DriverManager.getConnection(url, username, password);
-			Statement istruzione = connection.createStatement();
-		) {
-			for (Element tabella : tabelle) {
+        Elements tabelle = doc.select("table[class$=\"_table\"]");
 
-				Element docente = tabella.select("thead tr:first-child").first();
-				TableSpanManager gestioneSpazi =new TableSpanManager();
+        for (Element tabella : tabelle) {
+            Element docente = tabella.select("thead tr:first-child").get(0);
+            System.out.println(docente.text());
+        }
 
-				nomeDocente = docente.text();
-				Elements righe = tabella.select("tbody tr");
-				int nRiga=0;
-				for (Element riga : righe) {
-					Elements celle = riga.select("td");
-					for(Element cella : celle) {
-						if(cella.select(".empty").size()==0&&cella.select(".break").size()==0&&!cella.text().isEmpty() && !cella.text().contains("Orario prodotto con")) {
-							int nColonna=gestioneSpazi.primaLibera(nRiga);
-							gestioneSpazi.occupa(nRiga, nColonna);
-							classe=cella.select(".studentsset").text();
-							aula=cella.select(".room").text();
-							if (!classe.isEmpty() && !aula.isEmpty()) {
-							insert = "INSERT INTO orario (professore, aula, giorno, ora, classe) VALUES ('" + nomeDocente + "','" + aula + "','" + nColonna + "','" + nRiga + "','" + classe + "')";
+        System.out.println("Inserisci il nome del docente per visualizzare l'orario:");
+        String nomeDocente;
+        String url = getValore("TOTEM_DATABASE", "jdbc:mysql://10.1.0.52:3306/totem");
+        String username = "totem";
+        String password = "totem";
 
-							istruzione.executeUpdate(insert);
-							}
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement istruzione = connection.createStatement()) {
 
-							if (cella.hasAttr("rowspan")) {
-								int rowspanValue = Integer.parseInt(cella.attr("rowspan"));
-								if (rowspanValue == 2) {
-									gestioneSpazi.occupa(nRiga + 1, nColonna);
-									if (!classe.isEmpty() && !aula.isEmpty()) {
-									insert = "INSERT INTO orario (professore, aula, giorno, ora, classe) VALUES ('" + nomeDocente + "','" + aula + "','" + nColonna + "','" + (nRiga+1) + "','" + classe + "')";
-									istruzione.executeUpdate(insert);
-									}
-								}
-							}
-						}else {
-							int nColonna=gestioneSpazi.primaLibera(nRiga);
-							gestioneSpazi.occupa(nRiga, nColonna);
-						}
+            for (Element tabella : tabelle) {
+                Element docente = tabella.select("thead tr:first-child").first();
+                TableSpanManager gestioneSpazi = new TableSpanManager();
 
+                nomeDocente = docente.text();
+                Elements righe = tabella.select("tbody tr");
+                int nRiga = 0;
 
-					}
-					nRiga++;
-				}
+                for (Element riga : righe) {
+                    Elements celle = riga.select("td");
+                    for (Element cella : celle) {
+                        if (cella.select(".empty").size() == 0 && cella.select(".break").size() == 0 && !cella.text().isEmpty() && !cella.text().contains("Orario prodotto con")) {
+                            int nColonna = gestioneSpazi.primaLibera(nRiga);
+                            gestioneSpazi.occupa(nRiga, nColonna);
+                            String classe = cella.select(".studentsset").text();
+                            String aula = cella.select(".room").text();
 
+                            if (!classe.isEmpty() && !aula.isEmpty()) {
+                                String[] classiArray = classe.replaceAll("[\\(\\)\\[\\]]", "").split(",");
+                                for (String singolaClasse : classiArray) {
+                                    singolaClasse = singolaClasse.trim();
+                                    String insert = "INSERT INTO orario (professore, aula, giorno, ora, classe) VALUES ('" + nomeDocente + "','" + aula + "','" + nColonna + "','" + nRiga + "','" + singolaClasse + "')";
+                                    istruzione.executeUpdate(insert);
+                                }
+                                
+                            
+                                if (cella.hasAttr("rowspan")) {
+                                    int rowspanValue = Integer.parseInt(cella.attr("rowspan"));
+                                    if (rowspanValue == 2) {
+                                        gestioneSpazi.occupa(nRiga + 1, nColonna);
+                                        if (!classe.isEmpty() && !aula.isEmpty()) {
+                                            for (String singolaClasse : classiArray) {
+                                                singolaClasse = singolaClasse.trim();
+                                                String insert = "INSERT INTO orario (professore, aula, giorno, ora, classe) VALUES ('" + nomeDocente + "','" + aula + "','" + nColonna + "','" + (nRiga + 1) + "','" + singolaClasse + "')";
+                                                istruzione.executeUpdate(insert);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
 
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                            
+                        } else {
+                            int nColonna = gestioneSpazi.primaLibera(nRiga);
+                            gestioneSpazi.occupa(nRiga, nColonna);
+                        }
+                    }
+                    nRiga++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
