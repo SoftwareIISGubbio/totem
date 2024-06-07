deff58136c6a1aa1425db8e7d6c36a77ed0aa8f3
jonathanpanico <jonathan.panico@iisgubbio.edu.it>
2024-06-07 12:03:56 +0200

    aggiunto crossOrigin

--- servertotem/src/main/java/it/edu/iisgubbio/servertotem/bottelegram/ImmaginiManager.java
+++ servertotem/src/main/java/it/edu/iisgubbio/servertotem/bottelegram/ImmaginiManager.java
@@ -17,8 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin
 @RestController
 
 public class ImmaginiManager {
--- web/m_bot/script.js
+++ web/m_bot/script.js
@@ -1,10 +1,15 @@
 "use strict";
 
-function m_bot(id){
+async function m_bot(id){
     let botBox = document.getElementById(id);
+
+    let risposta = await fetch("http://10.1.0.52:8080/notizie?N=1");
+    let elenco = risposta.json();
+    let urlImmagine = "http://10.1.0.52:8080/immagini/" + elenco[0].tag;
+
     botBox.innerHTML = `
        <p>ultime notizie</p>
-       <img src="wip.png" alt="lavori in corso" width="100">
+       <img src="${urlImmagine}" alt="lavori in corso" style="height: 45vh">
        `;
     botBox.style.cssText = `
         padding:1em;
