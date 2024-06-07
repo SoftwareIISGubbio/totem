cb7df5f23a5e485252ee982a3149e2ce236c8ce9
jonathanpanico <jonathan.panico@iisgubbio.edu.it>
2024-06-07 12:15:35 +0200

    grafica delle foto

--- web/m_bot/script.js
+++ web/m_bot/script.js
@@ -4,15 +4,16 @@ async function m_bot(id){
     let botBox = document.getElementById(id);
 
     let risposta = await fetch("http://10.1.0.52:8080/notizie?N=1");
-    let elenco = risposta.json();
+    let elenco = await risposta.json();
+    console.log(elenco);
     let urlImmagine = "http://10.1.0.52:8080/immagini/" + elenco[0].tag;
+    
 
     botBox.innerHTML = `
-       <p>ultime notizie</p>
-       <img src="${urlImmagine}" alt="lavori in corso" style="height: 45vh">
-       `;
+       <img src="${urlImmagine}" alt="lavori in corso" style="height: 40vh">
+       <p>${elenco[0].descrizione}</p>`;
     botBox.style.cssText = `
-        padding:1em;
+        padding:1em;text-align:center;
     `;
 
 }
